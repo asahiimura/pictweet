0e51b9b8b583c551a52251401529cf2b12dcc804
asahiimura <58813834+asahiimura@users.noreply.github.com>
2020-05-29 11:55:38 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
-    respond_to do |format|
-      format.html { redirect_to tweet_path(params[:tweet_id])  }
-      format.json
-    end
+    # respond_to do |format|
+    #   format.html { redirect_to tweet_path(params[:tweet_id])  }
+    #   format.json
+    # end
   end
 
   private
